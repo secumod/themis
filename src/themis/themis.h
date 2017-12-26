636ccb9b5be83841955e8b01f8af45e9567cb362
Ignat Korchagin <ignat.korchagin@gmail.com>
2017-12-26 18:10:01 +0000

    Reorder includes in themis.h, so the error definition comes first

--- src/themis/themis.h
+++ src/themis/themis.h
@@ -32,11 +32,12 @@
 
 #define THEMIS_VERSION_TEXT "themis 0.9: "
 
+#include <themis/themis_error.h>
+
 #include <themis/secure_cell.h>
 #include <themis/secure_comparator.h>
 #include <themis/secure_message.h>
 #include <themis/secure_session.h>
-#include <themis/themis_error.h>
 
 const char *themis_version(void);
 
